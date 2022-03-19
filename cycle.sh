b53f786657a3682bf742f97007c02f6b2c85b662
Brett Viren <brett.viren@gmail.com>
2022-03-19 17:30:03 -0400

    More honing

--- cycle.sh
+++ cycle.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-spit="$(dirname "$BASE_SOURCE")"
+# This variable is referred to in .yaml files.
+export spit="$(dirname $(realpath "$BASH_SOURCE"))"
     
+if [ ! -f "$spit/scope/repos.yaml" ] ; then
+    cat <<EOF > "$spit/scope/repos.yaml"
+repos:
+  - $spit/scope/repo
+  - \$spack/var/spack/repos/builtin
+EOF
+fi
+
 spack () {
     PKG_CONFIG_PATH=/usr/share/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig "$spit/spack/bin/spack" -C "$spit/scope" $@
 }
 
-view=$spidir/view
 
-spack view rm -a $view
-spack uninstall -ya spit
+
+view=$spit/view
+
+spack view rm -a $view   || echo "it's okay"
+spack uninstall -ya spit || echo "it's okay"
 spack   install     spit
 spack view add -i $view spit
